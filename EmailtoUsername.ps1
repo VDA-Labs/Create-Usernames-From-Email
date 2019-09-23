0848257f740074e30f17155f86bfc8fd39d530b5
VDAAustin <55412947+VDAAustin@users.noreply.github.com>
2019-09-23 09:56:17 -0400

    Update EmailtoUsername.ps1

--- EmailtoUsername.ps1
+++ EmailtoUsername.ps1
@@ -6,20 +6,29 @@ $Email = @()
 $Username = @()
 $Name = @() 
 $Names = @()
-$Domain = "@Company.com"
-$CSVLocation = "C:\Temp\company.csv"
+$Domain = "haworth"
+$TLD = ".com"
+$CSVLocation = "C:\Temp\HaworthUsername.csv"
+$UsernameatDomain = @()
+$UsernameNoDomain = @()
+$DomainSlashUsername = @()
 
+#Import CSV file of email addresses and passwords
 $EmailPasswordRaw = Import-CSV $CSVLocation
+#Remove Passwords from the list
 $EmailRaw = $EmailPasswordRaw.Username
 
 
-
+#Remove the domain from the email address
 Foreach ($Email in $EmailRaw)
 {
 $EmailUsernames += $Email.Split('@')[0]
 
 }
 
+
+
+#Split the email address into a firstname and lastname
 Foreach  ($Emailusername in $EmailUsernames) 
 {
 $Names += $Emailusername | ForEach-Object {
@@ -27,10 +36,19 @@ $Names += $Emailusername | ForEach-Object {
 }
 }
 
+
+#Builds username based on internal username structure
 Foreach ($Name in $Names)
 {
-$Username += $Name.Firstname.Substring(0,1) + $Name.Lastname + $Domain
-
+#For use in logins that require the domain name in Name@domainformat
+$UsernameatDomain += $Name.Firstname.Substring(0,1) + $Name.Lastname + "@" + $Domain + $TLD
+#For use in logins that do not require the domain name
+$UsernameNoDomain += $Name.Firstname.Substring(0,1) + $Name.Lastname
+#For use in logs that use domain/username
+$DomainSlashUsername += $Domain + "/" + $Name.Firstname.Substring(0,1) + $Name.Lastname
 }
 
-$Username
+#Show the various formats that have been created
+$UsernameatDomain[1]
+$UsernameNoDomain[1]
+$DomainSlashUsername[1]
